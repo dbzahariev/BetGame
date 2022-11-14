1830ad868b80cbe560901002f8a61f8279fd9157
Dimitar Zahariev <dbzahariev@gmail.com>
2022-11-15 01:59:09 +0200

    Fix onChange function

--- client/src/components/AutoRefresh.tsx
+++ client/src/components/AutoRefresh.tsx
@@ -56,8 +56,10 @@ export default function AutoRefresh({ refresh }: { refresh: Function }) {
                 max={5 * 60}
                 defaultValue={30}
                 value={newInterval === "disable" ? 30 : newInterval}
-                onChange={(value: number) => {
-                  setNewInterval(value);
+                onChange={(value?: number | null) => {
+                  if (value) {
+                    setNewInterval(value);
+                  }
                 }}
                 bordered={false}
               />
