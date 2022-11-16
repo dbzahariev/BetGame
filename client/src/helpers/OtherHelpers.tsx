8b686617c816fc210d5dc288e305b85dbe08a33e
Dimitar Zahariev <dbzahariev@gmail.com>
2022-11-16 16:34:24 +0200

    Add React to other helper

--- client/src/helpers/OtherHelpers.tsx
+++ client/src/helpers/OtherHelpers.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line
+import React from "react"
 import axios, { AxiosRequestConfig } from "axios";
 import { selectedCompetition, selectedApiVersion } from "../App";
 import $ from "jquery";
