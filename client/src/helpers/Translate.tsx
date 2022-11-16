06bfe7d20cb7342a5b6df403a5934a85b0c95867
Dimitar Zahariev <dbzahariev@gmail.com>
2022-11-16 12:39:11 +0200

    Fix translate

--- client/src/helpers/Translate.tsx
+++ client/src/helpers/Translate.tsx
@@ -3,7 +3,8 @@ let teams = require("./teams.json");
 export const translateTeamsName = (team: string): string => {
   let teamsToShow: string = teams[team];
   if ((team || "").toLocaleLowerCase().indexOf("group") > -1) {
-    let teamNameArr = team.split(" ");
+    let teamNameArr = team.split("_");
+    console.log('fffff', teamNameArr)
     teamsToShow = `${teams[teamNameArr[0]]} ${teamNameArr[1]}`;
   }
   return teamsToShow ? teamsToShow : team;
