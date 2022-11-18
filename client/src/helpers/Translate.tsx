893649d0cda67f21fd2021995b2f1f127bd960ac
Dimitar Zahariev <dbzahariev@gmail.com>
2022-11-18 11:06:18 +0200

    Fix code

--- client/src/helpers/Translate.tsx
+++ client/src/helpers/Translate.tsx
@@ -4,7 +4,6 @@ export const translateTeamsName = (team: string): string => {
   let teamsToShow: string = teams[team];
   if ((team || "").toLocaleLowerCase().indexOf("group") > -1) {
     let teamNameArr = team.split("_");
-    console.log('fffff', teamNameArr)
     teamsToShow = `${teams[teamNameArr[0]]} ${teamNameArr[1]}`;
   }
   return teamsToShow ? teamsToShow : team;
