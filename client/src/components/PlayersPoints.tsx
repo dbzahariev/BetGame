92ad95096267b34ce94b754785335608abb4fc13
Dimitar Zahariev <dbzahariev@gmail.com>
2024-06-15 14:12:19 +0300

    remove debugger

--- client/src/components/PlayersPoints.tsx
+++ client/src/components/PlayersPoints.tsx
@@ -110,7 +110,6 @@ export default function PlayersPoints() {
       data = data.slice(0, data.length);
       let matches: MatchType[] = [];
 
-      debugger
       data.forEach((el, index) => {
         let score = el.score;
         let matchToAdd: MatchType = {
