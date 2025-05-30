47ca5f457272cf2520051014ee94994365631d3e
Dimitar Zahariev <dbzahariev@gmail.com>
2025-05-30 14:26:18 +0300

    remove react and change base href

--- client/src/app/all-matches/all-matches.ts
+++ client/src/app/all-matches/all-matches.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-all-matches',
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './all-matches.html',
   styleUrl: './all-matches.scss'
 })
-export class AllMatches {
+export class AllMatches implements OnInit {
+  matches: any[] = [];
+  constructor() {
+    console.log('AllMatches component initialized');
+  }
 
+  async ngOnInit() {
+
+    let res: any = await fetch('api/db/matches')
+      .then(response => response.json()).catch(error => {
+        debugger
+        return console.error('Error:', error)
+      })
+      .then(data2 => {
+        debugger;
+      })
+    debugger;
+    // const response = await fetch('api/db/matches');
+    // response.then((res) => {
+    //   debugger;
+    // })
+    // const res: any = await response.json();
+    // this.matches = res;
+    // console.log('AllMatches component onInit');
+  }
 }
