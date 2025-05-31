fd5d028fe82b497f2b9069f561ccefeaf58cf1fd
Dimitar Zahariev <dbzahariev@gmail.com>
2025-05-31 09:53:57 +0300

    try to fix 9

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 const { Server } = require('socket.io');
+const fs = require('fs');
 
-const headers = { 'Content-Type': 'application/json' };
+const headers = {
+  'Content-Type': 'application/json',
+  'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+  'Pragma': 'no-cache',
+  'Expires': '0',
+  'Surrogate-Control': 'no-store'
+};
 const server = http.createServer((req, res) => {
   if (req.url === '/api/db/matches' && req.method === 'GET') {
     const matches = [
@@ -14,10 +21,21 @@ const server = http.createServer((req, res) => {
     return;
   }
 
-  if (
-    (req.url === '/' && req.method === 'GET') ||
-    (req.url === '/api/hello' && req.method === 'GET')
-  ) {
+  if (req.url === '/' && req.method === 'GET') {
+    // Serve the Angular index.html for the root URL
+    fs.readFile(__dirname + '/client/src/index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+    return;
+  }
+
+  if (req.url === '/api/hello' && req.method === 'GET') {
     io.emit('message', 'A request was made to the server');
     res.writeHead(200, headers);
     res.end(JSON.stringify({ message: 'Hello, world!' }));
