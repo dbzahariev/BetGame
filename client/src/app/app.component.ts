5c21bf9a177c4a60f10879a1a3f50ff801cf71f4
Dimitar Zahariev <dbzahariev@gmail.com>
2025-05-31 12:08:26 +0300

    feat: implement Socket.IO client for real-time match updates

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { io, Socket } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,27 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'client';
+  private socket: Socket;
 
   constructor(private translate: TranslateService) {
     console.log('AppComponent initialized');
     translate.addLangs(['en', 'bg']);
     const storedLang = sessionStorage.getItem('currentLang') || "bg";
     this.translate.setDefaultLang(storedLang);
+
+    // Socket.IO client initialization
+    let url = window.location.origin.split(':')[0] + ':' + window.location.origin.split(':')[1]+ ':8080';
+    console.log('window.location.origin:', url);
+    this.socket = io(url);
+    this.socket.on('connect', () => {
+      console.log('Socket.IO connected:', this.socket.id);
+    });
+    this.socket.on('matches', (matches) => {
+      console.log('Получени мачове от сървъра:', matches);
+      // Тук може да се добави логика за обновяване на UI
+    });
+    this.socket.on('disconnect', () => {
+      console.log('Socket.IO disconnected');
+    });
   }
 }
